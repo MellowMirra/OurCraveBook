92dc7b161d52e8e4037f4e78e1188db22eba7839
MellowMirra <calhounm79@gmail.com>
2025-08-28 11:34:23 -0600

    Create firebase-config.js
    Fire config

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9cwZdJSMOQ6qw_0sRIdgdS7PPzqrVu5g",
+  authDomain: "ourcravebook.firebaseapp.com",
+  projectId: "ourcravebook",
+  storageBucket: "ourcravebook.firebasestorage.app",
+  messagingSenderId: "500658571362",
+  appId: "1:500658571362:web:e5f4acbb872f5a08e2a93b",
+  measurementId: "G-C7Q3ZZLPG5"
+};
