4e12bfbb08b78486919e98a16544930195e854f8
MellowMirra <calhounm79@gmail.com>
2025-08-28 12:23:16 -0600

    Update firebase-config.js
    Up

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
-// Firebase Config Placeholder
-// Replace the config object with your own Firebase keys
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB9cwZdJSMOQ6qw_0sRIdgdS7PPzqrVu5g",
+  authDomain: "ourcravebook.firebaseapp.com",
+  projectId: "ourcravebook",
+  storageBucket: "ourcravebook.firebasestorage.app",
+  messagingSenderId: "500658571362",
+  appId: "1:500658571362:web:e5f4acbb872f5a08e2a93b",
+  measurementId: "G-C7Q3ZZLPG5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
